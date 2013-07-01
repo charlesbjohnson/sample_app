54ad7856d8c47861f90fa9bd56f08d50f6ceef72
Charles B Johnson <cbj767@outlook.com>
2013-07-01 18:03:16 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  match '/signup',  to: 'users#new', via: 'get'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
   root to: 'static_pages#home'
-  match '/signup',  to: 'users#new',           via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
   subject { page }
 
@@ -33,8 +33,7 @@ describe "UserPages" do
         before { click_button submit }
 
         it { should have_title('Sign up') }
-        it { should have_content('error') }
-        it { should have_selector('div.alert.alert-error') }
+        it { should have_error_message('error') }
       end
     end
 
@@ -54,8 +53,9 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
